c760d5ebba3385528f00d0a19bc62bc0d23adf6c
Boris <boris@durpes.com>
2024-04-18 16:39:19 -0400

    a lil bit more

--- script.js
+++ script.js
@@ -5,20 +5,28 @@ document.querySelector('nav ul').addEventListener('click', function(e) {
     }
 });
 
-// Fonction pour gérer l'affichage du bouton de retour en haut
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("backToTop").style.display = "block";
+// Fonction pour animer le défilement vers le haut
+function scrollToTop(scrollDuration) {
+    const scrollStep = -window.scrollY / (scrollDuration / 15),
+        scrollInterval = setInterval(function(){
+        if ( window.scrollY != 0 ) {
+            window.scrollBy( 0, scrollStep );
+        }
+        else clearInterval(scrollInterval); 
+    },15);
+}
+
+// Événement de clic pour le bouton
+document.getElementById('backToTop').addEventListener('click', function() {
+    scrollToTop(500); // 500 ms pour le défilement total
+});
+
+// Gestion de l'affichage du bouton
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 500) {
+        document.getElementById('backToTop').style.display = 'block';
     } else {
-        document.getElementById("backToTop").style.display = "none";
+        document.getElementById('backToTop').style.display = 'none';
     }
-}
+});
 
-// Fonction pour remonter en haut de la page
-function topFunction() {
-    document.body.scrollTop = 0; // Pour Safari
-    document.documentElement.scrollTop = 0; // Pour Chrome, Firefox, IE et Opera
-}
-
-// Attachement des événements
-window.onscroll = function() { scrollFunction() };
